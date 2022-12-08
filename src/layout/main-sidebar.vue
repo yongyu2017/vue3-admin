21580a58644ea40f410aa144bde0e2e2b8a490c9
yongyu2017 <286016906@qq.com>
2022-12-08 09:52:05 +0800

    feat
    1、新增首页导航菜单；

--- src/layout/main-sidebar.vue
+++ src/layout/main-sidebar.vue
@@ -2,6 +2,12 @@
     <div class="site-sidebar site-sidebar--dark">
         <div class="site-sidebar__inner">
             <el-menu class="site-sidebar__menu" :default-active="menuActiveName || 'home'">
+                <el-menu-item index="home" @click="router.push({ name: 'home' })">
+                    <template #title>
+                        <el-icon><House /></el-icon>
+                        <span>首页</span>
+                    </template>
+                </el-menu-item>
                 <mainSidebarSubMenu :menu="item" v-for="(item, index) in menuList" :key="index"></mainSidebarSubMenu>
             </el-menu>
         </div>
@@ -12,9 +18,11 @@
 import mainSidebarSubMenu from './main-sidebar-sub-menu'
 import { storeToRefs } from "pinia"
 import { useStorePinia } from '@/store'
+import { useRouter } from 'vue-router'
 
 const store = useStorePinia()
 let { menuList, menuActiveName } = storeToRefs(store)
+const router = useRouter()
 </script>
 
 <style scoped>
