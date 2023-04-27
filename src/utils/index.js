31593b1f8dd8eaa829e4f054fc1bdcdd38132de6
yongyu2017 <286016906@qq.com>
2023-04-27 19:01:29 +0800

    perf
    1、代码优化；

--- src/utils/index.js
+++ src/utils/index.js
@@ -149,3 +149,17 @@ export function secondToTime (time) {
         date: zeroFill(h) + ':'+ zeroFill(m) + ':' + zeroFill(s)
     }
 }
+
+// blob文件流下载
+export function downloadForBlob (file, filename) {
+    const blob = new Blob([file])
+
+    let downloadElement = document.createElement('a');
+    let href = window.URL.createObjectURL(blob);           //创建下载的链接
+    downloadElement.href = href;
+    downloadElement.download = filename; //下载后文件名
+    document.body.appendChild(downloadElement);
+    downloadElement.click();                               //点击下载
+    document.body.removeChild(downloadElement);            //下载完成移除元素
+    window.URL.revokeObjectURL(href);                      //释放掉blob对象
+}
--- src/views/tool/ffmpeg/index.vue
+++ src/views/tool/ffmpeg/index.vue
@@ -3,27 +3,28 @@
         <el-form ref="dataFormRef" :model="dataForm" :rules="dataRule" label-position="left" label-width="100px">
             <el-form-item label="上传文件：" prop="file">
                 <el-upload
-                        :auto-upload="false"
-                        :show-file-list="false"
-                        :on-change="fileUpload"
+                    v-loading="ajaxLoading"
+                    :auto-upload="false"
+                    :show-file-list="false"
+                    :on-change="fileUpload"
                 >
                     <el-button type="primary">选择文件</el-button>
                 </el-upload>
             </el-form-item>
             <el-form-item label="帧数：" prop="frameRate">
                 <el-input-number
-                        v-model="dataForm.frameRate"
-                        :min="0"
-                        :step="1"
-                        :precision="2"
-                        controls-position="right"
-                        size="large"
-                        class="inp-dom inp-number-dom"
+                    v-model="dataForm.frameRate"
+                    :min="0"
+                    :step="1"
+                    :precision="2"
+                    controls-position="right"
+                    size="large"
+                    class="inp-dom inp-number-dom"
                 />
                 <el-popover
-                        placement="top-start"
-                        :width="200"
-                        trigger="hover"
+                    placement="top-start"
+                    :width="200"
+                    trigger="hover"
                 >
                     <div>-r rate：帧速率(fps) （可以改，确认非标准桢率会导致音画不同步，所以只能设定为15或者29.97）</div>
                     <template #reference>
@@ -33,25 +34,59 @@
             </el-form-item>
             <el-form-item label="宽度：" prop="width">
                 <el-input-number
-                        v-model="dataForm.width"
-                        :min="0"
-                        :step="1"
-                        :precision="0"
-                        controls-position="right"
-                        size="large"
-                        class="inp-dom inp-number-dom"
+                    v-model="dataForm.width"
+                    :min="0"
+                    :step="1"
+                    :precision="0"
+                    controls-position="right"
+                    size="large"
+                    class="inp-dom inp-number-dom"
                 />
+                <el-popover
+                        placement="top-start"
+                        :width="200"
+                        trigger="hover"
+                >
+                    <div style="display: flex">
+                        <div>常用分辨率：</div>
+                        <div style="flex: 1">
+                            <div v-for="(item, index) in resolutionList" :key="index">
+                                <el-button type="primary" link @click="setVideoResolution(item)">{{ item }}</el-button>
+                            </div>
+                        </div>
+                    </div>
+                    <template #reference>
+                        <el-button type="danger" link class="tips-btn"><el-icon><Warning /></el-icon></el-button >
+                    </template>
+                </el-popover>
             </el-form-item>
             <el-form-item label="高度：" prop="height">
                 <el-input-number
-                        v-model="dataForm.height"
-                        :min="0"
-                        :step="1"
-                        :precision="0"
-                        controls-position="right"
-                        size="large"
-                        class="inp-dom inp-number-dom"
+                    v-model="dataForm.height"
+                    :min="0"
+                    :step="1"
+                    :precision="0"
+                    controls-position="right"
+                    size="large"
+                    class="inp-dom inp-number-dom"
                 />
+                <el-popover
+                        placement="top-start"
+                        :width="200"
+                        trigger="hover"
+                >
+                    <div style="display: flex">
+                        <div>常用分辨率：</div>
+                        <div style="flex: 1">
+                            <div v-for="(item, index) in resolutionList" :key="index">
+                                <el-button type="primary" link @click="setVideoResolution(item)">{{ item }}</el-button>
+                            </div>
+                        </div>
+                    </div>
+                    <template #reference>
+                        <el-button type="danger" link class="tips-btn"><el-icon><Warning /></el-icon></el-button >
+                    </template>
+                </el-popover>
             </el-form-item>
             <el-form-item label="开始时间：" prop="rangeStart">
                 <el-input v-model="dataForm.rangeStart" placeholder="请输入" class="inp-dom"></el-input>
@@ -71,7 +106,7 @@
                 {{ zoneTime }}秒
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="dataFormSubmit">开始转换</el-button>
+                <el-button type="primary" v-loading="ajaxLoading" @click="dataFormSubmit">开始转换</el-button>
             </el-form-item>
         </el-form>
 
@@ -102,32 +137,27 @@
                 </div>
             </div>
 
-            <div class="video-box">
-                <video id="video" controls style="width: 100%" v-if="downloadFileUrl"></video>
+            <div class="video-box" v-if="downloadFileUrl">
+                <video id="video" controls style="width: 100%"></video>
                 <div>
-                    <a :href="downloadFileUrl" download="put.mp4">下载</a>
+                    <el-button type="primary" :icon="Download" link @click="downloadFun">下载</el-button>
                 </div>
             </div>
         </div>
     </div>
-
-    <!--<div class="home">-->
-        <!--<input type="file" id="upload" ACCEPT="video/*" @change="upload">-->
-        <!--<p id="text">{{ msg }}</p>-->
-        <!--<div>开始时间：{{ startTime }}</div>-->
-        <!--<div>结束时间：{{ endTime }}</div>-->
-        <!--<div>用时：{{ zoneTime }}秒</div>-->
-    <!--</div>-->
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted, computed, nextTick } from 'vue'
+import { Download } from '@element-plus/icons-vue'
 import { ElMessage } from 'element-plus'
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
-import { loadJS, getSuffix, secondToTime } from '@/utils/index'
+import { loadJS, getSuffix, secondToTime, downloadForBlob } from '@/utils/index'
 const dayjs = require('dayjs')
 
 let ffmpeg = null
+let st = null
+let FFmpegResult = null
 const progress = ref(0)
 const startTime = ref('')
 const endTime = ref('')
@@ -147,10 +177,11 @@ const dataForm = ref({
 })
 const dataRule = ref({})
 const uploadFileType = ['mp4', 'avi', 'flv', 'webm', 'mpeg', '3gp']
-const fileTypeList = ['mp4', 'avi', 'webm', 'mpeg', 'flv']
+const fileTypeList = ['mp4', 'avi', 'webm', 'mpeg', 'flv', 'gif']
+const resolutionList = ['720x480', '1280x720', '1920x1080', '2048x1080', '4096x2160']
 const fileInfo = ref([])
 const activeName = ref('Index0')
-let st = null
+const ajaxLoading = ref(false)
 
 const zoneTime = computed(() => {
     return startTime.value && endTime.value ? dayjs(endTime.value).diff(startTime.value, 'second') : 0
@@ -170,6 +201,7 @@ onUnmounted(() => {
     clearInterValFun()
 })
 
+// js动态加载
 const loadJSFun = () => {
     loadJS('./static/mediainfo/mediainfo.js', () => {
         // eslint-disable-next-line
@@ -181,6 +213,7 @@ const loadJSFun = () => {
         })
     })
 }
+// 文件上传
 const fileUpload = (e) => {
     const file = e.raw
     const fileType = getSuffix(file.name)[1]
@@ -189,11 +222,15 @@ const fileUpload = (e) => {
         return
     }
 
+    FFmpegResult = null
+    progress.value = 0
+    startTime.value = ''
+    endTime.value = ''
     downloadFileUrl.value = ''
     dataForm.value.file = file
     getVideoInfo(file)
-    // FFmpegToTranscoding(file)
 }
+// 获取文件信息
 const getVideoInfo = (file) => {
     const getSize = () => file.size
     const readChunk = (chunkSize, offset) =>
@@ -229,7 +266,6 @@ const getVideoInfo = (file) => {
             dataForm.value.width = Number(videoInfo.Width)
             dataForm.value.height = Number(videoInfo.Height)
             dataForm.value.rangeEnd = secondToTime(Math.floor(Number(videoInfo.Duration))).date
-            console.log(fileInfo.value, dataForm.value)
         })
         .catch((error) => {
             console.log(error)
@@ -252,46 +288,78 @@ const initFFmpeg = async () => {
     }
 }
 // FFmpeg业务处理
-const FFmpegToTranscoding = async (file) => {
-    clearInterValFun()
-    st = setInterval(() => {
-        endTime.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
-    }, 1000)
-    const { name } = file
-    // ffmpeg模块加载，需放在业务需要时，否则会造成内存泄漏问题
-    if (!ffmpeg.isLoaded()) {
-        await ffmpeg.load()
-    }
-    startTime.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
-    ffmpeg.FS('writeFile', name, await fetchFile(file))
-    const outputName = 'put.' + dataForm.value.fileType
-    await ffmpeg.run('-i', name, '-r', dataForm.value.frameRate + '', '-ss', dataForm.value.rangeStart, '-to', dataForm.value.rangeEnd, outputName)
-    const data = ffmpeg.FS('readFile', outputName)
-    // await ffmpeg.run('-i', name, '-r', '35', '-filter:v', 'setpts=0.25*PTS', '-b:v', '5m', 'put.mp4')
-    // const data = ffmpeg.FS('readFile', 'put.mp4')
+const FFmpegToTranscoding = (file) => {
+    // eslint-disable-next-line
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { name } = file
+            const outputName = 'put.' + dataForm.value.fileType
+            // ffmpeg模块加载，需放在业务需要时，否则会造成内存泄漏问题
+            if (!ffmpeg.isLoaded()) {
+                await ffmpeg.load()
+            }
+            ffmpeg.FS('writeFile', name, await fetchFile(file))
+            await ffmpeg.run('-i', name, '-r', dataForm.value.frameRate + '', '-ss', dataForm.value.rangeStart, '-to', dataForm.value.rangeEnd, '-s', dataForm.value.width + '*' + dataForm.value.height, outputName)
+            // await ffmpeg.run('-i', name, '-r', '35', '-filter:v', 'setpts=0.25*PTS', '-b:v', '5m', 'put.mp4')
+            // const data = ffmpeg.FS('readFile', 'put.mp4')
 
-    downloadFileUrl.value = URL.createObjectURL(new Blob([data.buffer], {
-        type: 'video/mp4'
-    }))
-    activeName.value = ''
-    nextTick(() => {
-        const video = document.getElementById('video')
-        video.src =  downloadFileUrl.value
+            resolve({
+                code: 200,
+                data: {
+                    ArrayBuffer: ffmpeg.FS('readFile', outputName),
+                    outputName,
+                },
+            })
+        } catch (e) {
+            reject({
+                code: -1,
+                message: e,
+            })
+        }
     })
 }
+// 清空定时器
 const clearInterValFun = () => {
     if (st) {
         clearInterval(st)
         st = null
     }
 }
-const dataFormSubmit = () => {
+const dataFormSubmit = async () => {
     if (!dataForm.value.file) {
         ElMessage.warning('请上传文件！')
         return
     }
 
-    FFmpegToTranscoding(dataForm.value.file)
+    startTime.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    endTime.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    clearInterValFun()
+    st = setInterval(() => {
+        endTime.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    }, 1000)
+    ajaxLoading.value = true
+    FFmpegResult = await FFmpegToTranscoding(dataForm.value.file)
+    console.log(FFmpegResult)
+    ajaxLoading.value = false
+    if (FFmpegResult.code == 200) {
+        downloadFileUrl.value = URL.createObjectURL(new Blob([FFmpegResult.data.ArrayBuffer]))
+        activeName.value = ''
+        nextTick(() => {
+            const video = document.getElementById('video')
+            video.src =  downloadFileUrl.value
+        })
+    } else {
+        downloadFileUrl.value = ''
+    }
+}
+// 文件下载
+const downloadFun = () => {
+    downloadForBlob(FFmpegResult.data.ArrayBuffer, FFmpegResult.data.outputName)
+}
+// 设置常用分辨率
+const setVideoResolution = (data) => {
+    dataForm.value.width = data.split('x')[0]
+    dataForm.value.height = data.split('x')[1]
 }
 </script>
 
